37a589166e4bb0a1aa18947be85adf957fe1ec2e
lwerner1 <46603318+lwerner1@users.noreply.github.com>
2020-07-09 00:46:59 -0300

    Add files via upload
    Decomposição LDL testada e funcionando
    447 e 857 do Burden

--- Ep2/ldl_decomp.py
+++ Ep2/ldl_decomp.py
@@ -14,22 +14,22 @@ def ldl_decomp(n, A_array, L_array, D_array):
 			L_array[i][j] = A_array[i][j]/D_array[i][i]
 	for i in range(0, n):
 		soma = 0
-		for j in range(0, i-1):
-			v[j] = L_array[i][j] * D_array[i][i]
+		for j in range(0, i):
+			v[j] = L_array[i][j] * D_array[j][j]
 			soma = soma + L_array[i][j] * v[j]
 		D_array[i][i] = A_array[i][i] - soma
-		for j in range(i, n):
+		for j in range(0, n):
 			soma1 = 0
-			for k in range (0, i-1):
+			for k in range (0, i):
 				soma1 = soma1 + L_array[j][k] * v[k]
 			L_array[j][i] = (A_array[j][i] - soma1)/D_array[i][i]
 
 
-A_array = [[1, 1, 4], [1, 5, 7], [4, 7, 9]]
-L_array = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
-D_array = [[1, 1, 1], [1, 1, 1], [1, 1, 1]]
+A_array = [[4, 1, 1, 1], [1, 3, -1, 1], [1, -1, 2, 0], [1, 1, 0, 2]]
+L_array = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
+D_array = [[1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1], [1, 1, 1, 1]]
 
-ldl_decomp(3, A_array, L_array, D_array)
+ldl_decomp(4, A_array, L_array, D_array)
 
 pprint.pprint(L_array)
 pprint.pprint(D_array)
